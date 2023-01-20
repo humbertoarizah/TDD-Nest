ca6eef6e46daafb8e7b0c6c45f59fea1fb0350ba
humbertoarizahsl <102244335+humbertoarizahsl@users.noreply.github.com>
2023-01-20 12:14:41 -0500

    adding entities

--- src/banks/entities/bank.entity.ts
+++ src/banks/entities/bank.entity.ts
@@ -1,1 +1,14 @@
-export class Bank {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ObjectId } from 'mongoose';
+@Schema({
+  timestamps: true,
+})
+export class Bank extends Document {
+  _id: ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+  @Prop({ type: String, required: true, maxlength: 50 })
+  name: string;
+}
+
+export const BankSchema = SchemaFactory.createForClass(Bank);
--- src/vendor/entities/vendor.entity.ts
+++ src/vendor/entities/vendor.entity.ts
@@ -1,1 +1,25 @@
-export class Vendor {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ObjectId, PopulatedDoc, Types } from 'mongoose';
+import { Bank } from 'src/banks/entities/bank.entity';
+@Schema({
+  timestamps: true,
+})
+export class Vendor extends Document {
+  _id: ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+  @Prop({ type: String, required: true })
+  name: string;
+  @Prop({ type: String, required: false }) // TODO: Regex
+  id: string;
+  @Prop({ type: String, required: true })
+  contactName: string;
+  @Prop({ type: String, required: false, maxlength: 10 })
+  mobile: string;
+  @Prop({ type: Types.ObjectId, required: true, ref: 'Bank' })
+  bank: ObjectId | PopulatedDoc<Bank>;
+  @Prop({ type: String, required: false, maxlength: 15 })
+  bankAccount: string;
+}
+
+export const VendorSchema = SchemaFactory.createForClass(Vendor);
