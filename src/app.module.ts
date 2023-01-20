5c8ef78a03b4475d98e515abfbb27705a4ae37bd
humbertoarizahsl <102244335+humbertoarizahsl@users.noreply.github.com>
2023-01-20 10:13:50 -0500

    Adding vendor and banks entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BanksModule } from './banks/banks.module';
+import { VendorModule } from './vendor/vendor.module';
 
 @Module({
-  imports: [],
+  imports: [BanksModule, VendorModule],
   controllers: [AppController],
   providers: [AppService],
 })
