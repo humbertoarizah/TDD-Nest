7bffe25752338b25a3ab0cf8299beedcc989e071
humbertoarizahsl <102244335+humbertoarizahsl@users.noreply.github.com>
2023-01-20 12:15:21 -0500

    adding basic files for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { BanksModule } from './banks/banks.module';
 import { VendorModule } from './vendor/vendor.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/dev'),
     BanksModule,
     VendorModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
