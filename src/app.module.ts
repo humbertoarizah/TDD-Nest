001545da0baaf9a305e533777f197ebfbd0f7533
humbertoarizahsl <102244335+humbertoarizahsl@users.noreply.github.com>
2023-01-20 23:07:25 -0500

    cleaning not needed files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { BanksModule } from './banks/banks.module';
 import { VendorModule } from './vendor/vendor.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-
+import EnvConfig from './config/app.config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfig],
+    }),
     MongooseModule.forRoot('mongodb://localhost:27017/dev'),
     BanksModule,
     VendorModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
