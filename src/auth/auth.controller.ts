f78c53d5a988477b95f06b69122e0b45b6b1e75e
humbertoarizahsl <102244335+humbertoarizahsl@users.noreply.github.com>
2023-01-20 23:08:12 -0500

    updating vendor dto and small cleaning in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,8 +8,6 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
--- src/vendor/dto/create-vendor.dto.ts
+++ src/vendor/dto/create-vendor.dto.ts
@@ -1,1 +1,20 @@
-export class CreateVendorDto {}
+import { IsMongoId, IsOptional, IsString } from 'class-validator';
+import { ObjectId } from 'mongoose';
+
+export class CreateVendorDto {
+  @IsString()
+  name: string;
+  @IsString()
+  @IsOptional()
+  id: string;
+  @IsString()
+  contactName: string;
+  @IsString()
+  @IsOptional()
+  mobile: string;
+  @IsMongoId()
+  bank: ObjectId;
+  @IsString()
+  @IsOptional()
+  bankAccount: string;
+}
